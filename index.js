b61c24c64a0219b3736f224c48dc024782b4d162
Nelson Sinis <nelsonsinis0@gmail.com>
2019-05-08 23:10:30 -0300

    Implemented package.json generator

--- index.js
+++ index.js
@@ -1,39 +1,76 @@
 #!/usr/bin/env node
-
 const inquirer = require('inquirer');
+const fs = require('fs');
+const shell = require('shelljs');
 
 const questions = [
   {
-    type: 'list',
-    name: 'technology',
-    message: 'Select a technology',
-    choices: ['API', 'Serverless'],
+    name: 'name',
+    message: 'What is project name?',
+  },
+  {
+    name: 'description',
+    message: 'Type a description:'
+  },
+  {
+    name: 'author',
+    message: 'Who is the author?'
+  },
+  {
+    name: 'entryPoint',
+    message: 'entry point:',
   },
   {
     type: 'list',
-    name: 'cloud',
-    message: 'Select a cloud platform',
-    choices: ['AWS', 'Azure'],
-  },
-  {
-    type: 'list',
-    name: 'database',
-    message: 'Select the ODM/ORM',
+    name: 'databaseTechnology',
+    message: 'Choose which database package',
     choices: ['Sequelize', 'Mongoose'],
   },
 ];
 
-let responses = {};
+inquirer.prompt(questions).then(responses => {
+  if (!shell.which('npm')) {
+    shell.echo('Sorry, this script requires npm installed');
+    shell.exit(1);
+  }
+  
+  if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git installed');
+    shell.exit(1);
+  }
 
-inquirer.prompt(questions).then(response => {
-  responses = {...response};
-  return inquirer.prompt([{
-    type: 'list',
-    name: 'test',
-    message: 'Select the ODM/ORM',
-    choices: [ "Choice A", new inquirer.Separator(), "choice B" ],
-  }]);
-}).then(response => {
-  responses = Object.assign(responses, response);
-  console.log(responses);
+  let packageJSON = {
+    name: responses.name,
+    description: responses.description,
+    author: responses.author,
+    version: '1.0.0',
+    main: responses.entryPoint,
+    license: 'MIT'
+  };
+
+  switch(responses.databaseTechnology) {
+    case 'Sequelize':
+      packageJSON = Object.assign(packageJSON, {
+        dependencies: {
+          sequelize: '^5.8.5',
+        },
+        devDependencies: {
+          'sequelize-cli': '^5.4.0'
+        },
+      });
+      break;
+    case 'Mongoose':
+      packageJSON.dependencies = {
+        mongoose: '^5.5.6'
+      };
+      break;
+    default:
+      break;
+  }
+
+  fs.mkdirSync(`./${responses.name}`);
+  fs.writeFileSync(`./${responses.name}/package.json`, JSON.stringify(packageJSON));
+  shell.cd(`./${responses.name}`);
+  shell.exec('npm install');
+  shell.exec('git init');
 }).catch(console.error);
